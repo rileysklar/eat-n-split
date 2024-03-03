dbce9a645070fc038fbb08376f023c2ca86810a8
Riley Sklar <riley.sklar@gmail.com>
2024-03-02 21:11:48 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -19,24 +21,36 @@ const initialFriends = [
   },
 ];
 
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="main-container">
       <Header />
       <div className="app">
         <div className="sidebar">
           <FriendsList />
-          <FormAddFriend />
-          <Button>Add Friend</Button>
+          {showAddFriend && <FormAddFriend />}
+          <Button onClick={handleShowAddFriend}>
+            {showAddFriend ? "Close" : "Add Friend"}
+          </Button>
         </div>
         <FormSplitBill />
       </div>
     </div>
   );
-}
-
-function Button({ children }) {
-  return <button className="button">{children}</button>;
 }
 
 function Header() {
